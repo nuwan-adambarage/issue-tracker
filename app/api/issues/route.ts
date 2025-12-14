9c0d4b3d00097a7982400fd7088085317faddfeb
Nuwan Adambarage <nuwan.adambarage2@unisuper.com.au>
2025-12-15 06:44:38 +1100

    Build the API for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
+
+import { prisma } from "@/prisma/client";
+
+const issueSchema = z.object({
+    title: z.string().min(3),
+    description: z.string().min(10)
+});
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const validation = issueSchema.safeParse(body);
+    if(!validation.success) {
+        return NextResponse.json({ error: validation.error.issues }, { status: 400 });
+    }
+
+    const newIssue = await prisma.issue.create({
+        data: { title: body.title, description: body.description }
+    })
+    return NextResponse.json(newIssue, { status: 201 });
+    
+}
