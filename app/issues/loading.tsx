6bcdd38404926161c6ec6220bd5fada4269aba0f
Nuwan Adambarage <nuwan.adambarage2@unisuper.com.au>
2025-12-20 06:34:10 +1100

    Add loading skeleton

---
+++ app/issues/loading.tsx
@@ -0,0 +1,37 @@
+import { Table } from '@radix-ui/themes'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import IssueStatusBadge from '../components/IssueStatusBadge'
+import IssueActions from './IssueActions'
+
+const LoadingIssuePage = () => {
+    const issues = [1,2,3,4,5];
+  return (
+    <div className='max-w-4xl'>
+        <IssueActions />
+        <SkeletonTheme baseColor="black" borderRadius="full" highlightColor="grey">
+            <Table.Root variant='surface'>
+            <Table.Header>
+            <Table.Row>
+                <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+            </Table.Row>
+            </Table.Header>
+            <Table.Body>
+            {issues.map((issue) => (
+
+                <Table.Row key={issue}>
+                <Table.Cell><Skeleton /></Table.Cell>
+                <Table.Cell className='hidden md:table-cell'><Skeleton /></Table.Cell>
+                <Table.Cell className='hidden md:table-cell'><Skeleton /></Table.Cell>
+                </Table.Row>
+            ))}
+            </Table.Body>
+        </Table.Root>
+      </SkeletonTheme>
+    </div>
+  )
+}
+
+export default LoadingIssuePage
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,17 +1,18 @@
 import { prisma } from '@/prisma/client'
 import { Button, Table, } from '@radix-ui/themes'
 import Link from 'next/link'
-import React from 'react'
 import IssueStatusBadge from '../components/IssueStatusBadge'
+import delay from 'delay';
+import IssueActions from './IssueActions';
 
 const IssuesPage = async () => {
 
   const issues = await prisma.issue.findMany();
+  await delay(2000);
+
   return (
     <div className='max-w-4xl'>
-      <div className='mb-5'>
-      <Button><Link href="/issues/new">New Issue</Link></Button>
-      </div>
+      <IssueActions />
       <Table.Root variant='surface'>
         <Table.Header>
           <Table.Row>
