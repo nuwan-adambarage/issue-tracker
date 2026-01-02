0864c7545f2122953634df818e2eae182c6dd665
Nuwan Adambarage <nuwan.adambarage2@unisuper.com.au>
2026-01-03 06:42:06 +1100

    Change the layout of the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Box } from '@radix-ui/themes';
+import { Box, Container, Flex } from '@radix-ui/themes';
 import classNames from 'classnames';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link'
@@ -14,22 +14,33 @@ const NavBar = () => {
     { href: '/dashboard', label: 'Dashboard' },
   ];
   return (
-    <nav className="flex bg-gray-800 p-4 text-white">
-      <Link href="/" className="mr-4 flex items-center"><FaBug /></Link>
-      <ul className="flex space-x-4">
-        {links.map(link => (
-          <li key={link.href}>
-            <Link href={link.href} className={classNames(
-                'mr-4 hover:text-gray-400 transition-colors',
-                { 'text-gray-400': currentPath === link.href }
-                )}>{link.label}</Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === 'unauthenticated' && <Link href="/api/auth/signin">Sign In</Link>}
-        {status === 'authenticated' && <Link href="/api/auth/signout">Sign Out ({session.user?.name})</Link>}
-      </Box>
+    <nav className="bg-gray-800 p-4 text-white">
+      <Container>
+      <Flex justify="between">
+        <Flex>
+          <Link href="/" className="mr-4 flex items-center"><FaBug /></Link>
+          <ul className="flex space-x-4">
+            {links.map(link => (
+              <li key={link.href}>
+                <Link href={link.href} className={classNames(
+                    'mr-4 hover:text-gray-400 transition-colors',
+                    { 'text-gray-400': currentPath === link.href }
+                    )}>{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </Flex>
+        <Box>
+          {status === 'unauthenticated' && <Link href="/api/auth/signin">Sign In</Link>}
+          {status === 'authenticated' && 
+          <Link href="/api/auth/signout">
+            Sign Out
+            <span className='hidden md:inline'>({session.user?.name})</span>
+          </Link>}
+        </Box>    
+      </Flex>
+      
+      </Container>
     </nav>
   )
 }
