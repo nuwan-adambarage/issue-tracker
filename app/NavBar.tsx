f0ead336a8eb033a45f73870a0d0239b801850de
Nuwan Adambarage <nuwan.adambarage2@unisuper.com.au>
2026-01-04 06:42:32 +1100

    Add Skeleton to SignIn

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,6 +5,7 @@ import { useSession } from 'next-auth/react';
 import Link from 'next/link'
 import { usePathname } from 'next/navigation';
 import { FaBug } from "react-icons/fa";
+import { Skeleton } from "@/app/components";
 
 const NavBar = () => {
   
@@ -53,7 +54,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
   const {status, data: session} = useSession();
 
-  if (status === 'loading') return null;
+  if (status === 'loading') return <Skeleton width="4rem" />;
   if (status === 'unauthenticated') return <Link className='nav-link' href="/api/auth/signin">Sign In</Link>
   return (
   <Box>    
