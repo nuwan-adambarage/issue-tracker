adf7f6788e48d215be437f063a9ed0917ba1911e
Nuwan Adambarage <nuwan.adambarage2@unisuper.com.au>
2026-01-04 08:44:56 +1100

    Populate AssigneeSelect component from backend data

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,26 @@
+'use client';
+import { User } from '@/app/generated/prisma/client'
 import { Select } from '@radix-ui/themes'
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 
 const AssigneeSelect = () => {
+    const [users, setUsers] = useState<User[]>([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const {data} = await axios.get<User[]>('/api/users');
+            setUsers(data);
+        }
+        fetchUsers();
+    }, [])
   return (
     <Select.Root>
 	<Select.Trigger placeholder='Assign...' />
 	<Select.Content>
 		<Select.Group>
 			<Select.Label>Suggestions</Select.Label>
-			<Select.Item value="1">Nuwan</Select.Item>
-			<Select.Item value="2">Madu</Select.Item>
-            <Select.Item value="3">Ramin</Select.Item>			
+            {users.map(user => <Select.Item key={user.id} value={user.email!}>{user.name}</Select.Item>)}			
 		</Select.Group>		
 	</Select.Content>
 </Select.Root>
