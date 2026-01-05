385ec2e821b727b044712c32d23fc2ffc01d2b0d
Nuwan Adambarage <nuwan.adambarage2@unisuper.com.au>
2026-01-06 06:15:41 +1100

    Enhanced the Issues PATCH API to asign issue to user

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,52 +1,83 @@
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/validationSchemas";
+import { issueSchema, patchIssueSchema } from "@/app/validationSchemas";
 import { prisma } from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
-    request: NextRequest,
-    { params }: { params: { id: string }}
+  request: NextRequest,
+  { params }: { params: { id: string } }
 ) {
-    const session = await getServerSession(authOptions)
-    if(!session) {
-        return NextResponse.json({ error: "Unauthenticated user" }, { status: 401 })
-    }
-        
-    const { id } = await params;
+  //   const session = await getServerSession(authOptions);
+  //   if (!session) {
+  //     return NextResponse.json(
+  //       { error: "Unauthenticated user" },
+  //       { status: 401 }
+  //     );
+  //   }
 
-    const body = await request.json();
-    const validation = issueSchema.safeParse(body);
-    if (!validation.success) {
-        return NextResponse.json({ errors: validation.error.format }, { status: 400 });
-    }
+  const { id } = await params;
 
-    const updatedIssue = await prisma.issue.update({
-        where: { id: Number(id) },
-        data: { title: body.title, description: body.description }
+  const body = await request.json();
+
+  const validation = patchIssueSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(
+      { errors: validation.error.message },
+      { status: 400 }
+    );
+  }
+
+  const { title, description, assignToUserId } = body;
+  if (assignToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignToUserId },
     });
-    return NextResponse.json(updatedIssue, { status: 200 });
+    if (!user)
+      return NextResponse.json({ errors: "Invalid user" }, { status: 400 });
+  }
+
+  const issue = await prisma.issue.findUnique({ where: { id: Number(id) } });
+  if (!issue) {
+    return NextResponse.json(
+      { errors: "Issue can not be found" },
+      { status: 404 }
+    );
+  }
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: Number(id) },
+    data: {
+      title,
+      description,
+      assignToUserId,
+    },
+  });
+  return NextResponse.json(updatedIssue, { status: 200 });
 }
 
 export async function DELETE(
-    request: NextRequest,
-    { params }: { params: { id: string }}
+  request: NextRequest,
+  { params }: { params: { id: string } }
 ) {
-    const session = await getServerSession(authOptions)
-    if(!session) {
-        return NextResponse.json({ error: "Unauthenticated user" }, { status: 401 })
-    }
-    const { id } = await params;
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json(
+      { error: "Unauthenticated user" },
+      { status: 401 }
+    );
+  }
+  const { id } = await params;
 
-    const issue = await prisma.issue.findUnique({
-        where: { id: Number(id) }
-    });
-    if (!issue) {
-        return NextResponse.json({ message: 'Issue not found' }, { status: 404 });
-    }
+  const issue = await prisma.issue.findUnique({
+    where: { id: Number(id) },
+  });
+  if (!issue) {
+    return NextResponse.json({ message: "Issue not found" }, { status: 404 });
+  }
 
-    await prisma.issue.delete({
-        where: { id: Number(id) }
-    });
-    return NextResponse.json({ message: 'Issue deleted' }, { status: 200 });
+  await prisma.issue.delete({
+    where: { id: Number(id) },
+  });
+  return NextResponse.json({ message: "Issue deleted" }, { status: 200 });
 }
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,28 @@
 import z from "@/node_modules/zod/v4/classic/external.cjs";
 
 export const issueSchema = z.object({
-    title: z.string().min(3, "Title must be at least 3 characters long"),
-    description: z.string().min(10, "Description must be at least 10 characters long")
+  title: z.string().min(3, "Title must be at least 3 characters long").max(255),
+  description: z
+    .string()
+    .min(10, "Description must be at least 10 characters long")
+    .max(65535),
+});
+
+export const patchIssueSchema = z.object({
+  title: z
+    .string()
+    .min(3, "Title must be at least 3 characters long")
+    .max(255)
+    .optional(),
+  description: z
+    .string()
+    .min(10, "Description must be at least 10 characters long")
+    .max(65535)
+    .optional(),
+  assignToUserId: z
+    .string()
+    .min(1, "Assigning user Id is required")
+    .max(255)
+    .optional()
+    .nullable(),
 });
