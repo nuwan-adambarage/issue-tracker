109ec8a8782c259f5eb9cf889aecd3f2a0b86638
Nuwan Adambarage <nuwan.adambarage2@unisuper.com.au>
2026-01-17 05:44:36 +1100

    Make coluns sortable

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,16 +4,26 @@ import { Table } from "@radix-ui/themes";
 import { CustomLink, IssueStatusBadge } from "@/app/components";
 import delay from "delay";
 import IssueActions from "./IssueActions";
-import { Status } from "../generated/prisma/client";
+import { Issue, Status } from "../generated/prisma/client";
+import NextLink from "next/link";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
 
 interface Props {
   searchParams: {
     status: Status;
+    orderBy: keyof Issue;
   };
 }
 
+const columns: { label: string; value: keyof Issue; className?: string }[] = [
+  { label: "Title", value: "title" },
+  { label: "Status", value: "status", className: "hidden md:table-cell" },
+  { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+];
+
 const IssuesPage = async ({ searchParams }: Props) => {
-  const { status } = await searchParams;
+  const { status, orderBy } = await searchParams;
+  // const params = {...searchParams};
 
   const statuses = Object.values(Status);
 
@@ -30,13 +40,18 @@ const IssuesPage = async ({ searchParams }: Props) => {
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
+            {columns.map(async (column) => (
+              <Table.ColumnHeaderCell key={column.value}>
+                <NextLink href={{
+                  query: {...await searchParams, orderBy: column.value}
+                }}>
+                  {column.label}
+                </NextLink>
+                {column.value === orderBy && (
+                  <ArrowUpIcon className="inline" />
+                )}
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
