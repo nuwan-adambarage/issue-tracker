fcb2866e7edab86b7e6f79ff6ca5e6c0f48b9b00
Nuwan Adambarage <nuwan.adambarage2@unisuper.com.au>
2026-01-18 08:27:45 +1100

    Implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import { use } from "react";
 import Pagination from "./components/Pagination";
 
-export default function Home() {
-  return (
-    <Pagination itemCount={100} pageSize={10} currentPage={1} />
+interface Props {
+  searchParams: Promise<{
+    page: string;
+  }>;
+}
+export default function Home({ searchParams }: Props) {
+  const params = use(searchParams);
+  const page = params.page ?? '1'; 
+  console.log('searchParams', page);
+  return (    
+    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(page)} />
   );
 }
