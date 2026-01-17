bc592f29634e7bb4b9040b2dfc8324bc019a0b6c
Nuwan Adambarage <nuwan.adambarage2@unisuper.com.au>
2026-01-18 07:19:15 +1100

    Build the layout of the paginaion component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+import Pagination from "./components/Pagination";
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Pagination itemCount={100} pageSize={10} currentPage={1} />
   );
 }
