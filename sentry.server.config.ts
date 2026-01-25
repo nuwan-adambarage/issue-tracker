7a34aaae9653ee5e4a6902c19c0a86c35f9196ac
Nuwan Adambarage <nuwan.adambarage2@unisuper.com.au>
2026-01-26 07:23:58 +1100

    Set up error tracking

---
+++ sentry.server.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://64b2e5259bbe6173477ed71bd5652d3a@o4510772881326080.ingest.us.sentry.io/4510772891811840",
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
