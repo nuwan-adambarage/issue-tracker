41f2fd072a7f493e756550540f4c69b005f80099
Nuwan Adambarage <nuwan.adambarage2@unisuper.com.au>
2026-06-29 11:40:48 +1000

    IssueStatusFilter inside a Suspense

--- app/issues/IssueActions.tsx
+++ app/issues/IssueActions.tsx
@@ -1,11 +1,14 @@
 import { Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
+import { Suspense } from "react";
 import IssueStatusFilter from "./IssueStatusFilter";
 
 const IssueActions = () => {
   return (
     <Flex justify="between">
-      <IssueStatusFilter />
+      <Suspense>
+        <IssueStatusFilter />
+      </Suspense>
       <Button>
         <Link href="/issues/new">New Issue</Link>
       </Button>
